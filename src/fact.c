112347b1832c80106e1cc9bb316d8f326fc8f945
Dima Krugliatsov <didimka2404@gmail.com>
2026-05-12 18:28:47 +0300

    Initial commit

---
+++ src/fact.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorial(int n) {
+    if (n < 0) {
+        fprintf(stderr, "Negative input not allowed\n");
+        exit(EXIT_FAILURE);
+    }
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int num;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+    printf("%d! = %d\n", num, factorial(num));
+    return 0;
+}
